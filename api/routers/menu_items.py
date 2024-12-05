7d5f0cf053cce52c53005d3f84f5f4813be297c1
Justus Schism <jschism@uncc.edu>
2024-12-05 15:18:40 -0500

    added menu_items.py router for endpoints to create, update, and delete menu items

---
+++ api/routers/menu_items.py
@@ -0,0 +1,35 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from ..dependencies.database import get_db
+from ..models import menu_items as models
+from ..schemas import menu_items as schemas
+
+router = APIRouter()
+
+@router.post('/menu-items/', response_model=schemas.MenuItem)
+def create_menu_item(item: schemas.MenuItemCreate, db: Session = Depends(get_db)):
+    db_item = models.MenuItems(**item.dict())
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+@router.put('/menu-items/{item_id}', response_model=schemas.MenuItem)
+def update_menu_item(item_id: int, item: schemas.MenuItemUpdate, db: Session = Depends(get_db)):
+    db_item = db.query(models.MenuItems).filter(models.MenuItems.id == item_id).first()
+    if db_item is None:
+        raise HTTPException(status_code=404, detail='Menu item not found')
+    for key, value in item.dict(exclude_unset=True).items():
+        setattr(db_item, key, value)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+@router.delete('/menu-items/{item_id}', response_model=schemas.MenuItem)
+def delete_menu_item(item_id: int, db: Session = Depends(get_db)):
+    db_item = db.query(models.MenuItems).filter(models.MenuItems.id == item_id).first()
+    if db_item is None:
+        raise HTTPException(status_code=404, detail='Menu item not found')
+    db.delete(db_item)
+    db.commit()
+    return db_item
